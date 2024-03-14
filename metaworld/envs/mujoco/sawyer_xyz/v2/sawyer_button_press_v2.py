31ba7e185e3e6466912d00cff8ab09d45618578e
zhaojing <victoryang1026@gmail.com>
2024-03-14 01:01:14 -0700

    add weight variants in  environment & training configs

--- metaworld/envs/mujoco/sawyer_xyz/v2/sawyer_button_press_v2.py
+++ metaworld/envs/mujoco/sawyer_xyz/v2/sawyer_button_press_v2.py
@@ -1,14 +1,19 @@
+import mujoco
 import numpy as np
-from gym.spaces import Box
+from gymnasium.spaces import Box
 
 from metaworld.envs import reward_utils
 from metaworld.envs.asset_path_utils import full_v2_path_for
-from metaworld.envs.mujoco.sawyer_xyz.sawyer_xyz_env import SawyerXYZEnv, _assert_task_is_set
+from metaworld.envs.mujoco.sawyer_xyz.sawyer_xyz_env import (
+    SawyerXYZEnv,
+    _assert_task_is_set,
+)
 
 
 class SawyerButtonPressEnvV2(SawyerXYZEnv):
-    def __init__(self):
-
+    prev = None
+    weights = [1, 1, 1, 1]
+    def __init__(self, tasks=None, render_mode=None):
         hand_low = (-0.5, 0.40, 0.05)
         hand_high = (0.5, 1, 0.5)
         obj_low = (-0.1, 0.85, 0.115)
@@ -18,15 +23,19 @@ class SawyerButtonPressEnvV2(SawyerXYZEnv):
             self.model_name,
             hand_low=hand_low,
             hand_high=hand_high,
+            render_mode=render_mode,
         )
 
+        if tasks is not None:
+            self.tasks = tasks
+
         self.init_config = {
-            'obj_init_pos': np.array([0., 0.9, 0.115], dtype=np.float32),
-            'hand_init_pos': np.array([0, 0.4, 0.2], dtype=np.float32),
+            "obj_init_pos": np.array([0.0, 0.9, 0.115], dtype=np.float32),
+            "hand_init_pos": np.array([0, 0.4, 0.2], dtype=np.float32),
         }
         self.goal = np.array([0, 0.78, 0.12])
-        self.obj_init_pos = self.init_config['obj_init_pos']
-        self.hand_init_pos = self.init_config['hand_init_pos']
+        self.obj_init_pos = self.init_config["obj_init_pos"]
+        self.hand_init_pos = self.init_config["hand_init_pos"]
         goal_low = self.hand_low
         goal_high = self.hand_high
 
@@ -38,7 +47,7 @@ class SawyerButtonPressEnvV2(SawyerXYZEnv):
 
     @property
     def model_name(self):
-        return full_v2_path_for('sawyer_xyz/sawyer_button_press.xml')
+        return full_v2_path_for("sawyer_xyz/sawyer_button_press.xml")
 
     @_assert_task_is_set
     def evaluate_state(self, obs, action):
@@ -48,17 +57,27 @@ class SawyerButtonPressEnvV2(SawyerXYZEnv):
             tcp_open,
             obj_to_target,
             near_button,
-            button_pressed
+            button_pressed,
         ) = self.compute_reward(action, obs)
+        # (
+        #     reward, avg_sum, tcp_height, tcp_vel, tcp_to_obj
+        # ) = self.compute_reward_v2(action, obs)
+
+        # info = {
+        #     "avg_sum": avg_sum,
+        #     "tcp_height": tcp_height,
+        #     "tcp_vel": tcp_vel,
+        #     "tcp_to_obj": tcp_to_obj
+        # }
 
         info = {
-            'success': float(obj_to_target <= 0.02),
-            'near_object': float(tcp_to_obj <= 0.05),
-            'grasp_success': float(tcp_open > 0),
-            'grasp_reward': near_button,
-            'in_place_reward': button_pressed,
-            'obj_to_target': obj_to_target,
-            'unscaled_reward': reward,
+            "is_success": float(obj_to_target <= 0.02),
+            "near_object": float(tcp_to_obj <= 0.05),
+            "grasp_success": float(tcp_open > 0),
+            "grasp_reward": near_button,
+            "in_place_reward": button_pressed,
+            "obj_to_target": obj_to_target,
+            "unscaled_reward": reward,
         }
 
         return reward, info
@@ -68,13 +87,13 @@ class SawyerButtonPressEnvV2(SawyerXYZEnv):
         return []
 
     def _get_id_main_object(self):
-        return self.unwrapped.model.geom_name2id('btnGeom')
+        return self.unwrapped.model.geom_name2id("btnGeom")
 
     def _get_pos_objects(self):
-        return self.get_body_com('button') + np.array([.0, -.193, .0])
+        return self.get_body_com("button") + np.array([0.0, -0.193, 0.0])
 
     def _get_quat_objects(self):
-        return self.sim.data.get_body_xquat('button')
+        return self.data.body("button").xquat
 
     def _set_obj_xyz(self, pos):
         qpos = self.data.qpos.flat.copy()
@@ -86,19 +105,18 @@ class SawyerButtonPressEnvV2(SawyerXYZEnv):
     def reset_model(self):
         self._reset_hand()
         self._target_pos = self.goal.copy()
-        self.obj_init_pos = self.init_config['obj_init_pos']
+        self.obj_init_pos = self.init_config["obj_init_pos"]
 
-        if self.random_init:
-            goal_pos = self._get_state_rand_vec()
-            self.obj_init_pos = goal_pos
-
-        self.sim.model.body_pos[
-            self.model.body_name2id('box')] = self.obj_init_pos
+        goal_pos = self._get_state_rand_vec()
+        self.obj_init_pos = goal_pos
+        self.model.body_pos[
+            mujoco.mj_name2id(self.model, mujoco.mjtObj.mjOBJ_BODY, "box")
+        ] = self.obj_init_pos
         self._set_obj_xyz(0)
-        self._target_pos = self._get_site_pos('hole')
+        self._target_pos = self._get_site_pos("hole")
 
         self._obj_to_target_init = abs(
-            self._target_pos[1] - self._get_site_pos('buttonStart')[1]
+            self._target_pos[1] - self._get_site_pos("buttonStart")[1]
         )
 
         return self._get_obs()
@@ -117,24 +135,76 @@ class SawyerButtonPressEnvV2(SawyerXYZEnv):
             tcp_to_obj,
             bounds=(0, 0.05),
             margin=tcp_to_obj_init,
-            sigmoid='long_tail',
+            sigmoid="long_tail",
         )
         button_pressed = reward_utils.tolerance(
             obj_to_target,
             bounds=(0, 0.005),
             margin=self._obj_to_target_init,
-            sigmoid='long_tail',
+            sigmoid="long_tail",
         )
 
         reward = 2 * reward_utils.hamacher_product(tcp_closed, near_button)
         if tcp_to_obj <= 0.05:
             reward += 8 * button_pressed
 
-        return (
-            reward,
-            tcp_to_obj,
-            obs[3],
-            obj_to_target,
-            near_button,
-            button_pressed
-        )
+        return (reward, tcp_to_obj, obs[3], obj_to_target, near_button, button_pressed)
+    
+    def compute_reward_v2(self, action, obs):
+        del action
+        obj = obs[4:7]
+        tcp = self.tcp_center
+        cur = tcp
+
+        # calculate height
+        tcp_height = tcp[2]
+
+        # velocity
+        if (self.prev is None):
+            self.prev = cur
+        tcp_vel = reward_utils.combined_velocity(cur[0], cur[1], cur[2], self.prev[0], self.prev[1], self.prev[2])
+        # print("tcp: ", tcp)
+
+        # distance to button
+        tcp_to_obj = np.linalg.norm(obj - tcp)
+
+        # avg sum of that
+        avg_sum = (tcp_height + tcp_vel + tcp_to_obj) / 3
+
+        # update
+        self.prev = tcp
+
+
+        
+        # compute actual reward
+        features = np.asarray([avg_sum, tcp_height, tcp_vel, tcp_to_obj])
+        weights = np.asarray(self.weights)
+        assert len(features.shape) == 1
+        assert len(weights.shape) == 1
+        reward = np.dot(features, weights)
+
+        return (reward, avg_sum, tcp_height, tcp_vel, tcp_to_obj)
+    
+    def set_variant(self, variant):
+        print("Setting weights to: " + str(variant['weights'][0]) + ", " + str(variant['weights'][1]) + ", " + str(variant['weights'][2]) + ", " + str(variant['weights'][3])  )
+        self.weights = variant['weights']
+
+
+class TrainButtonPressv2(SawyerButtonPressEnvV2):
+    tasks = None
+
+    def __init__(self):
+        SawyerButtonPressEnvV2.__init__(self, self.tasks)
+
+    def reset(self, seed=None, options=None):
+        return super().reset(seed=seed, options=options)
+
+
+class TestButtonPressv2(SawyerButtonPressEnvV2):
+    tasks = None
+
+    def __init__(self):
+        SawyerButtonPressEnvV2.__init__(self, self.tasks)
+
+    def reset(self, seed=None, options=None):
+        return super().reset(seed=seed, options=options)
---
+++ scripts/eval_sbx.py
@@ -0,0 +1,68 @@
+import os
+import numpy as np
+# from sbx import SAC
+from stable_baselines3 import SAC
+from stable_baselines3.common.save_util import load_from_zip_file
+import imageio
+import argparse
+import json
+
+from metaworld.envs import ALL_V2_ENVIRONMENTS_GOAL_OBSERVABLE
+
+
+def main(args):
+    # Init the environment
+    button_press_goal_observable_cls = ALL_V2_ENVIRONMENTS_GOAL_OBSERVABLE["button-press-v2-goal-observable"]
+    eval_env = button_press_goal_observable_cls(render_mode='rgb_array')
+    eval_env._freeze_rand_vec = False
+
+    # Opening JSON file for weight variants
+    cur_dir = os.path.dirname(os.path.abspath(__file__))
+    variant_path = os.path.abspath(os.path.join(cur_dir, "..", "training_configs/sawyer_button_press_v2/variants/", args.variant))
+    filename = "variant.json"
+
+    with open(os.path.join(variant_path, filename), 'r') as openfile:
+        variantFile = json.load(openfile)
+
+    eval_env.set_variant(variantFile["eval_environment_kwargs"])
+
+    # Load the trained agent
+    train_dir = os.path.join(cur_dir, "..", "training_results", args.variant)
+    data, params, pytorch_variables = load_from_zip_file(
+        os.path.join(train_dir, "sac_button_press_v2_goal_observable"),
+        device="auto",
+        )
+    model = SAC("MlpPolicy", eval_env, verbose=1, batch_size=500)
+    model.set_parameters(params, exact_match=True, device="auto")
+
+
+    # Evaluate the agent
+    images = []
+    success_count = 0
+    for i in range(10):
+        obs, _ = eval_env.reset()
+        img = eval_env.render()
+        for t in range(500):
+            action, _ = model.predict(obs)
+            obs, reward, done, info = eval_env.step(action)
+            # print(info['is_success'], info['obj_to_target'], reward)
+            if info['is_success']:
+                success_count += 1
+                break
+            if done:
+                eval_env.reset()
+                break
+            if i == 0:
+                images.append(img)
+                img = eval_env.render()
+
+    print("Success rate: ", success_count / 10)
+    imageio.mimsave(os.path.join(train_dir, "button_press_eval.gif"), [np.array(img) for i, img in enumerate(images)], fps=30)
+
+if __name__ == "__main__":
+    parser = argparse.ArgumentParser()
+    parser.add_argument("--variant", help="e.g., 0-1-1-2", type=str, default="0-0-0-0")
+
+    args = parser.parse_args()
+
+    main(args)
