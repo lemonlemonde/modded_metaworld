0209c414f68a665ff7c9fbf82943495a80372be0
lemonlemonde <mirudjun@gmail.com>
2024-04-24 04:12:16 +0000

    dataset!

--- scripts/retrieve_images.py
+++ scripts/retrieve_images.py
@@ -6,6 +6,8 @@ import argparse
 import pickle
 
 import cv2
+import torch
+from torchvision.transforms import v2
 
 def get_images_from_mp4s():
     # directory
@@ -15,6 +17,9 @@ def get_images_from_mp4s():
     # images npy array
     images = []
 
+    # error
+    defects = []
+
     # go through 0-0-0-0, 0-0-0-2, ..., 2-2-2-2
     for i in range(3):
         for j in range(3):
@@ -22,6 +27,7 @@ def get_images_from_mp4s():
                 for l in range(3):
                     variant = str(i) + "-" + str(j) + "-" + str(k) + "-" + str(l)
                     for m in range (4):
+                        print("Retrieving images for variant: " + variant + " trial: " + str(m))
                         # trial
                         # get the mp4 from the directory
                         mp4_path = os.path.join(trajectory_dir, variant, "images_" + str(m), "button_press_" + str(m) + ".mp4")
@@ -37,11 +43,16 @@ def get_images_from_mp4s():
                                 images.append(image)
                             count += 1
                         print("Done variant: ", variant, "trial: ", m, "frames: ", count)
+                        if (count != 502):
+                            print("Variant: ", variant, " trial: ", m, " doesn't have 512 frames!!!!!")
+                            defects.append("variant: ", variant, "trial: ", m)
 
+    for i in defects:
+        print(defects)
 
     # save images as npy
-    image_dir = os.path.join(cur_dir, "dataset")
-    np.save(os.path.join(image_dir, "images.npy"), images
+    image_dir = os.path.join(cur_dir, "../dataset")
+    np.save(os.path.join(image_dir, "images.npy"), images)
 
 if __name__ == '__main__':
     print("-->>-->>-- Retrieving images from mp4s! --<<--<<--")
